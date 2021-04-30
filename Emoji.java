b4dc769973c60051025d741ec47e5b80f3a12981
Maher <jillian.maher2@boone.kyschools.us>
2021-04-30 06:38:55 -0400

    Initial commit

---
+++ Emoji.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Emoji here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Emoji extends Actor
+{
+    /** STEP 3: Create instance variables */
+    
+    
+    
+    
+    /** STEP 4 and 5: Create constructor with two String parameters */
+    
+    
+    
+    
+    /**
+     * Act - do whatever the Emoji wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // When the mouse is click on this object, play the sound.
+        if(Greenfoot.mouseClicked(this))
+        {
+             /** STEP 6: Insert code below */
+             
+        }
+    }    
+}
---
+++ HomeworkWorld.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class HomeworkWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HomeworkWorld extends World
+{
+    /**  STEP 1: Create two arrays to store the image file names and the sound file names */
+    
+    
+    
+
+    
+    /**
+     * Constructor for objects of class HomeworkWorld.
+     * 
+     */
+    public HomeworkWorld()
+    {   
+        
+        // Create a new world with 400x100 cells with a cell size of 1x1 pixels.
+        super(400, 100, 1); 
+        
+        /**  STEP 2: Use a loop to place 5 Emojis on the World */
+        
+        
+        
+        
+        
+    }
+}
